99ce618160a1559fc1d12fbc8662fda35d22fae1
nunofilipemargarido <nunofilipemargarido@gmail.com>
2015-07-19 23:28:44 +0100

    version 1.0

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,12 @@
+exports.question=function(req, res, next) {
+  res.render('quizes/question', { pergunta : 'Capital de Italia' });
+}
+
+exports.answer=function(req, res, next) {
+	if(req.query.respuesta==="Roma"){
+		res.render('quizes/answer', { respuesta: 'Correcto' });
+	}else{
+		res.render('quizes/answer', { respuesta: 'Incorrecto' });
+	}
+  
+}
