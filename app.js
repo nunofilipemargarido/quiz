c896084ff54e65ab151c55d2b3dd018ce091e0dd
nunofilipemargarido <nunofilipemargarido@gmail.com>
2015-08-01 22:23:43 +0100

    version 20

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('env', 'development');
+
 
 app.use(partials());
 //app.use(express.static('/public/images'));
