8a111cfb2b51163ca96969436f1fd62377c370c1
nunofilipemargarido <nunofilipemargarido@gmail.com>
2015-08-01 22:10:04 +0100

    versao 18

--- models/models.js
+++ models/models.js
@@ -3,9 +3,24 @@ var path = require('path');
 //cargar modelos orm
 var Sequelize=require('sequelize');
 
+var url=process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name=(url[6]||null);
+var user   =(url[2]||null);
+var pwd    =(url[3]||null);
+var protocol=(url[1]||null);
+var dialect=(url[1]||null);
+var port =(url[5]||null);
+var host =(url[4]||null);
+var storage=process.env.DATABASE_STORAGE
+
 //Usar var bbdd ssqlite
-var sequelize=new Sequelize(null,null,null,{
-	dialect:"sqlite",storage:"quiz.sqlite"
+var sequelize=new Sequelize(DB_name,user,pwd,{
+	dialect:protocol,
+	protocol:protocol,
+	port:port,
+	host:host,
+	storage:storage,
+	omitNull:true
 }) ;
 
 //importar la defenicion de la tabela quiz en quiz.js
